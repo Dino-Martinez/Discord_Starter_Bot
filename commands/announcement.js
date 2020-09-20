94bd83f2d33c48733f6ed39e79dcbed9e04c38af
Secundino Martinez <Dino.Martinez.514@gmail.com>
2020-09-19 22:36:46 -0700

    added command to announce things in specified channels, pinging a specified role

---
+++ commands/announcement.js
@@ -0,0 +1,26 @@
+module.exports = {
+  name: 'announcement',
+  aliases: ['announce', 'announcements'],
+  description: 'Announces a message, pinging a specified role. YOU MUST SPECIFY THE ROLE BY PINGING IT'
+  + '\nFor example,"!ping #announcements @PingableRole This is my message."',
+  usage: '<Channel Name> <@Role Name> <Message>',
+  requiredArgs: 3,
+  maxArgs: 1000000,
+  cooldown: 5,
+  execute(message, args) {
+    const role = message.mentions.roles.first();
+    const channelName = args[0];
+    const announcement = args.slice(2).join(' ');
+
+    const yourchannel = message.guild.channels.cache
+      .find((channel) => channel.name === channelName);
+
+    if (role.name === 'everyone' || role.name === 'here') {
+      yourchannel.send('I cannot ping everyone or here with this command!');
+    } else {
+      yourchannel.send(`<@&${role.id}> : ${announcement}`);
+    }
+
+    return { action: 'none' };
+  },
+};
